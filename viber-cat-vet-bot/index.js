db67ea3b1e35ce3f36895dfd9b519b72381f22bc
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2021-12-22 11:49:45 +0200

    Initial commit

---
+++ viber-cat-vet-bot/index.js
@@ -0,0 +1,36 @@
+const ViberBot = require('viber-bot').Bot,
+    BotEvents = require('viber-bot').Events,
+    TextMessage = require('viber-bot').Message.Text,
+    express = require('express');
+const app = express();
+
+if (!"4e7600646567e092-f49608b799d24b2e-57b664085c36ff76") {
+    console.log('Could not find bot account token key.');
+    return;
+}
+if (!"https://28b8-212-1-76-102.ngrok.io") {
+    console.log('Could not find exposing url');
+    return;
+}
+
+const bot = new ViberBot({
+    authToken: "4e7600646567e092-f49608b799d24b2e-57b664085c36ff76",
+    name: "TestViberBot86",
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
+});
+bot.on(BotEvents.SUBSCRIBED, response => {
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
+});
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+    response.send(new TextMessage(`Message received.`));
+});
+const port = process.env.PORT || 8000;
+app.use("/viber/webhook", bot.middleware());
+app.listen(port, () => {
+    console.log(`Application running on port: ${port}`);
+    bot.setWebhook(`${"https://28b8-212-1-76-102.ngrok.io"}/viber/webhook`).catch(error => {
+        console.log('Can not set webhook on following server. Is it running?');
+        console.error(error);
+        process.exit(1);
+    });
+});
