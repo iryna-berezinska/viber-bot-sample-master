e01bc06f3fe38f3e15691bec00f82d7726f75506
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2021-12-24 16:04:26 +0200

    keyboard

--- viber-cat-vet-bot/index.js
+++ viber-cat-vet-bot/index.js
@@ -1,14 +1,17 @@
 const ViberBot = require('viber-bot').Bot,
     BotEvents = require('viber-bot').Events,
     TextMessage = require('viber-bot').Message.Text,
-    express = require('express');
+    express = require('express')
 const app = express();
 
+module.exports = {
+    Keyboard: require(__dirname + '/keyboard-message')
+};
 if (!"4e7600646567e092-f49608b799d24b2e-57b664085c36ff76") {
     console.log('Could not find bot account token key.');
     return;
 }
-if (!"https://a616-212-1-76-102.ngrok.io") {
+if (!"https://2d7e-212-1-76-102.ngrok.io") {
     console.log('Could not find exposing url');
     return;
 }
@@ -16,7 +19,7 @@ if (!"https://a616-212-1-76-102.ngrok.io") {
 const bot = new ViberBot({
     authToken: "4e7600646567e092-f49608b799d24b2e-57b664085c36ff76",
     name: "TestViberBot86",
-    avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
+    avatar: "https://hi-news.ru/wp-content/uploads/2014/09/291082_10150399764264169_32074004168_8065767_897175744_o1-750x563.jpg"
 });
 bot.on(BotEvents.SUBSCRIBED, response => {
     response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
@@ -24,13 +27,14 @@ bot.on(BotEvents.SUBSCRIBED, response => {
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     response.send(new TextMessage(`Привет`));
 });
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.use("/viber/webhook", bot.middleware());
 app.listen(port, () => {
     console.log(`Application running on port: ${port}`);
-    bot.setWebhook(`${"https://a616-212-1-76-102.ngrok.io"}/viber/webhook`).catch(error => {
+    bot.setWebhook(`${"https://2d7e-212-1-76-102.ngrok.io"}/viber/webhook`).catch(error => {
         console.log('Can not set webhook on following server. Is it running?');
         console.error(error);
         process.exit(1);
     });
 });
+
